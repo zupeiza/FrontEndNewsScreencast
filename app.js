4139a16226325ee38aa1631f897cb097797c6326
zupeiza <zupeiza@gmail.com>
2016-03-16 20:03:35 +0100

    Jquery added, liked and blink effect

---
+++ app.js
@@ -0,0 +1,13 @@
+//-----------------------------
+//	DOCUMENT READY
+//-----------------------------
+
+$(document).ready(function() {
+	$('.js-like').on('click', function(event) {
+		event.preventDefault();
+		$(this).text('Liked!')
+		 .closest('.news-item')
+		 .addClass('is-liked');
+	});
+});
+
