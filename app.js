1132a2c13a16fdd94d90ad43d63f8b5a6826273c
zupeiza <zupeiza@gmail.com>
2016-03-21 12:08:03 +0100

    modal insertion and animation

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 //-----------------------------
 
 $(document).ready(function() {
+
+	//-----------------------------
+	//	Like feature
+	//-----------------------------
 	$('.js-like').on('click', function(event) {
 		event.preventDefault();
 		$(this).text('Liked!')
@@ -10,9 +14,26 @@ $(document).ready(function() {
 		 .addClass('is-liked');
 	});
 
+	//-----------------------------
+	//	Form visibility
+	//-----------------------------
 	$('.js-add-link').on('click', function(event) {
 		event.preventDefault();
 		$('.js-form').toggleClass('is-visible');
+	});
+
+	//-----------------------------
+	//	Modal visibility
+	//-----------------------------
+	$('.js-show-modal').on('click', function(event) {
+		event.preventDefault();
+		$('.js-modal').addClass('is-visible');
+		$('.js-modal-overlay').addClass('is-visible');
+	});
+
+	$('.js-modal-overlay').on('click', function(event) {
+		$('.js-modal').removeClass('is-visible');
+		$('.js-modal-overlay').removeClass('is-visible');
 	});
 });
 
